87c7faac0e7e3044c9f53cdf5e1c305911416cf7
wintermeovv <1216086408@qq.com>
2025-03-07 17:25:49 +0800

    Update widget.config.js

--- conf/widget.config.js
+++ conf/widget.config.js
@@ -18,7 +18,7 @@ module.exports = {
   WIDGET_PET: process.env.NEXT_PUBLIC_WIDGET_PET || true, // 是否显示宠物挂件
   WIDGET_PET_LINK:
     process.env.NEXT_PUBLIC_WIDGET_PET_LINK ||
-    'https://raw.githubusercontent.com/imuncle/live2d/refs/heads/master/live2d_3/model/Azue%20Lane(JP)/dujiaoshou_4/dujiaoshou_4.model3.json', // 挂件模型地址 @see https://github.com/xiazeyu/live2d-widget-models
+    'https://raw.githubusercontent.com/wintermeovv/live2d/refs/heads/master/%E9%BB%91%E7%99%BD%E7%8C%AB%E7%8C%AB.model3.json', // 挂件模型地址 @see https://github.com/xiazeyu/live2d-widget-models
   WIDGET_PET_SWITCH_THEME:
     process.env.NEXT_PUBLIC_WIDGET_PET_SWITCH_THEME || false, // 点击宠物挂件切换博客主题
 
